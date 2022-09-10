f4ec16d8f43fe016ad638fce89e7109b53340787
Lydia Kim <lydiakim47@gmail.com>
2022-09-10 14:03:47 -0700

    package jsons added, src and lib folders added, requires on index.js added

---
+++ index.js
@@ -0,0 +1,7 @@
+const generateHTML = require("./src/generateHTML");
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+const manager = require("./lib/manager");
+const engineer = require("./lib/engineer");
+const intern = require("./lib/intern");
